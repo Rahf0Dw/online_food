a4daf0dc4db00f696ab3324f2b4674c4c1bba78c
rahaf douiar <rahafdw666@gmail.com>
2022-02-21 23:30:24 +0200

    =update orders

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> جميع الحقوق محفوظة  techcode24</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
---
+++ partials-front/footer.php
@@ -0,0 +1,26 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/%D8%A8%D8%B1%D9%85%D8%AC%D8%A9-%D9%85%D9%88%D8%A7%D9%82%D8%B9-%D8%A7%D9%84%D9%88%D9%8A%D8%A8-111936637251687" target="_blank"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCA81DmTOmbkJxubrVffhCBw" target="_blank" ><img src="https://img.icons8.com/fluent/48/000000/youtube.png"/></a>
+                </li>
+
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p style="font-size: 26px;" >تابعنا على مواقع التواصل الاجتماعي للمزيد من المشاريع</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
